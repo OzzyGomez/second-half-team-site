25846e9d4c37ff68a4870f41943098b56eee5146
Osvaldo villagomez <osvaldovillagomez@Osvaldos-MBP.attlocal.net>
2021-12-07 17:47:32 -0600

    more members

--- src/Pages/Home/Team/Members.tsx
+++ src/Pages/Home/Team/Members.tsx
@@ -14,13 +14,46 @@ export interface Member {
 
 const Members: Member[] = [
   {
-    name: 'Ozzy Gomez',
+    name: 'Vision',
     image: 'https://via.placeholder.com/300x300',
     title: 'Player',
     links: [
       {
         type: 'twitch',
         url: 'https://www.twitch.tv/OzzyGomez'
+      }
+    ]
+  },
+  {
+    name: 'Beezy',
+    image: 'https://via.placeholder.com/300x300',
+    title: 'Player',
+    links: [
+      {
+        type: 'twitch',
+        url: 'https://www.twitch.tv/beeezy420'
+      }
+    ]
+  },
+  {
+    name: 'Wongderful',
+    image: 'https://via.placeholder.com/300x300',
+    title: 'Player',
+    links: [
+      {
+        type: 'twitch',
+        url: 'https://www.twitch.tv/wongderful'
+      }
+    ]
+  },
+  {
+    name: 'MrTrevino',
+    image: 'https://via.placeholder.com/300x300',
+    title: 'Player',
+    links: [
+      {
+        type: 'twitch',
+        url: 'https://www.twitch.tv/mrtrevinoo'
       }
     ]
   }
